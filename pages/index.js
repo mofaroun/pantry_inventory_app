2a2e3657270e19d07091e02a11fc4f753a246ceb
mofaroun <farounmohammed@gmail.com>
2024-08-04 16:32:19 -0500

    Fixing the NPM run build error

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { db } from './firebase'; 
+import { db } from '../lib/firebase'; 
 import { collection, addDoc, getDocs, updateDoc, query, where, deleteDoc, doc } from 'firebase/firestore';
 import { 
   Box, 
